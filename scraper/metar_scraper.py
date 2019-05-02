cbb2b6fc9f9c13c42edff9852c9f3f00883e4e79
wApNo <vapenkapavel@gmail.com>
2019-05-02 22:59:37 +0200

    cleanup

---
+++ scraper/metar_scraper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import requests
+from bs4 import BeautifulSoup
+
+icao = input("Input AIRPORT ICAO code: ").upper()
+url = "https://tgftp.nws.noaa.gov/data/observations/metar/stations/"+ icao +".TXT"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text,"lxml")
+
+print(soup.text)
